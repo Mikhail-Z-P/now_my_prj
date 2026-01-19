72908bf82e2f2476a2e433dd9039e632149b656f
Михаил Змановский <999999zqq@gmail.com>
2026-01-19 22:27:20 +0700

    filter for russian names

--- src/quests.py
+++ src/quests.py
@@ -1,3 +1,6 @@
+import re
+
+
 def name_list(file_names: str) -> list:
     """ Функцыя убирает лишние знаки припинания и цфры """
     formatted_names_list = list()
@@ -12,7 +15,20 @@ def name_list(file_names: str) -> list:
                 formatted_names_list.append(new_name)
     return formatted_names_list
 
+def is_cirillic(name_item: str) ->bool:
+    """ Проверяет на вхождение кириллиы в строку """
+    return bool(re.search('[а-яА-Я]', name_item))
+
+
+def filteer_russian_names(names_list: list) -> list:
+    """ Филтрацыя имен написанных на русском """
+    new_names_list = list()
+    for name_item in names_list:
+        if is_cirillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
+
 if __name__ == '__main__':
     cleared_name = name_list('names.txt')
-    for i in cleared_name:
-        print(i)
+    print(filteer_russian_names(cleared_name))
