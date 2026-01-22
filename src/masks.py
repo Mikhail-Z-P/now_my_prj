13d2d43275a5aa3920aa057ef9d6ba8150257064
Михаил Змановский <999999zqq@gmail.com>
2026-01-22 17:44:36 +0700

    optimization of the get_mask_card_number

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_mask_card_number(bank_card: str) -> str:
     """Функцыя разделяет каждые четыре цыфры по блокам  и скрывает цыфры идущие посло 6 и до 4 с конца"""
 
@@ -5,15 +8,17 @@ def get_mask_card_number(bank_card: str) -> str:
     account = 0
     card_number = ""
 
-    for card in bank_card:
+    # Извлекаем только цифры из входной строки
+    digits = re.sub(r'\D', '', bank_card)
+
+    for card in digits:
         account += 1
-        if account <= 6 or account > 12:
+        if account <= 6 or account > len(digits) - 4:
             card_number += card
-        elif 12 >= account >= 6:
-            card = "*"
-            card_number += card
+        else:
+            card_number += "*"
         count_four += 1
-        if count_four % 4 == 0 and card != len(bank_card) - 1:
+        if count_four % 4 == 0 and account != len(digits):
             card_number += " "
 
     return str(card_number)
