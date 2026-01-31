defb6e01a2faac7956b8bb5c763947c3ad5f6cf2
Михаил Змановский <999999zqq@gmail.com>
2026-01-31 23:50:32 +0700

    создал и проверил работу функции sort_by_date

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from widget import mask_account_card, get_date
-from processing import filter_by_state
+from processing import filter_by_state, sort_by_date
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
@@ -8,3 +8,7 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
+
 def filter_by_state(list_dictionary, state="EXECUTED") -> list:
-    """Функцыя принимает список словарей и возвращяет только соответствующие ключу stat"""
+    """Функция принимает список словарей и возвращает только соответствующие ключу stat"""
 
     now_list_dictionary = []
 
@@ -9,9 +10,8 @@ def filter_by_state(list_dictionary, state="EXECUTED") -> list:
     return now_list_dictionary
 
 
+def sort_by_date(list_dictionary: list, sorting: bool = True) -> list:
+    """Функция сортирует список словарей по ключу date"""
 
-
-
-
-
-
+    sorted_list = sorted(list_dictionary, key=lambda x: x["date"], reverse=sorting)
+    return sorted_list
