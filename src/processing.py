bc732eeaf6b470dc001b12fbb0c62d0fee6f6206
Михаил Змановский <999999zqq@gmail.com>
2026-01-31 22:48:55 +0700

    создал и проверил работу функции filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,12 @@
-def filter_by_state(list_dictionaryб, stat="EXECUTED": list) -> list:
+def filter_by_state(list_dictionary, state="EXECUTED") -> list:
+    """Функцыя принимает список словарей и возвращяет только соответствующие ключу stat"""
+
+    now_list_dictionary = []
+
+    for dictionary in list_dictionary:
+        if dictionary["state"] == state:
+            now_list_dictionary.append(dictionary)
+    return now_list_dictionary
 
 
 
@@ -7,7 +15,3 @@ def filter_by_state(list_dictionaryб, stat="EXECUTED": list) -> list:
 
 
 
-print(filter_by_state[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
