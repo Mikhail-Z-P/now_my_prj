f2c78545fdd35dc4154057692480c3ebdbe9d55d
Михаил Змановский <999999zqq@gmail.com>
2026-02-04 02:40:50 +0700

    проверки

--- utils/file1.py
+++ utils/file1.py
@@ -1,27 +1,17 @@
-# print(
-#     filter_by_state(
-#         [
-#             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#         ]
-#     )
-# )
-# результат: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-#
-# print(
-#     sort_by_date(
-#         [
-#             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#         ]
-#     )
-# )
-# результат: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-#             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+def add_numbers(a, b):
+    return a + b
+
+
+def is_even(a):
+    return a % 2 == 0
+
+
+def find_max(my_list):
+    return max(my_list)
+
+if __name__ == "__main__":
+    assert add_numbers(2, 2) == 4
+
+    assert is_even(0) == True
+
+    assert find_max([1, 2, 3, 4, 5]) == 5
